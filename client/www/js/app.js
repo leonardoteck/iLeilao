e7a5987bdb184bc3b467abc23346989a8ddde98c
Caio Larroza <caiolarroza@hotmail.com>
2017-06-16 17:21:15 -0300

    Adicionado telas de solicitação e leilão

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -3,41 +3,110 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(run)
-    .config(configs);
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
-run.$inject = ['$ionicPlatform'];
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
 
-function run($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
 
-            // Don't remove this line unless you know what you are doing. It stops the viewport
-            // from snapping when text inputs are focused. Ionic handles this internally for
-            // a much nicer keyboard experience.
-            cordova.plugins.Keyboard.disableScroll(true);
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.solicitacoes', {
+    url: '/solicitacoes',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/solicitacoes.html'
+      }
+    }
+  })
+
+  .state('app.solicitacoes_part', {
+    url: '/solicitacoes_part',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/solicitacoes_part.html'
+      }
+    }
+  })
+
+  .state('app.acompanha', {
+      url: '/acompanha',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/acompanha.html'
         }
-        if (window.StatusBar) {
-            StatusBar.styleDefault();
+      }
+    })
+    .state('app.principal', {
+      url: '/principal',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/principal.html',
+          controller: 'PlaylistsCtrl'
         }
-    });
-}
+      }
+    })
 
-configs.$inject = ['$stateProvider', '$urlRouterProvider', '$ionicConfigProvider', '$httpProvider'];
+      .state('app.login', {
+      url: '/login',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/login.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
 
-function configs($stateProvider, $urlRouterProvider, $ionicConfigProvider, $httpProvider) {
+      .state('app.leiloei', {
+      url: '/leiloei',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/leiloei.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
 
-    //$stateProvider
-    //    .state('app', {
-    //        url: '/app',
-    //        abstract: true,
-    //        templateUrl: 'templates/menu.html'
-    //    });
 
-    //$urlRouterProvider.otherwise('/login');
-}
+    .state('app.andamento', {
+      url: '/andamento',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/andamento.html'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/principal');
+});
